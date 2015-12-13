775317cd82686c06df95154fa982f0cc6a944381
LeslieK <leslieklein@comcast.net>
2015-12-13 01:07:52 -0500

    logging_serial.py serializes log records from multiple processes into one file

--- flying-sheep/arguments.py
+++ flying-sheep/arguments.py
@@ -9,7 +9,7 @@ parser.add_argument("-t", "--torrent_file", type=str, default="Mozart_mininova.t
 
 # hostname default is None
 # default causes leecher to connect to tracker
-parser.add_argument("-n", "--hostname", type=str, default='',\
+parser.add_argument("-n", "--hostname", type=str, default='127.0.0.1',\
     help="ip address of a seeder peer")
 parser.add_argument("-d", "--remoteserverport", type=int, \
     help="port number of server on remote host")
---
+++ flying-sheep/logging_serial.py
@@ -0,0 +1,99 @@
+"""
+This module runs in a 'logging process' that serializes the log messages from the other processes.
+"""
+
+import logging
+import logging.handlers
+import multiprocessing
+
+import asyncio
+from main_bt import Client, TorrentWrapper, main, logger
+from arguments import remoteserverport, localserverport, seeder, hostname
+
+def listener_configurer():
+    root = logging.getLogger()
+    h = logging.handlers.RotatingFileHandler(filename='bittorrent.log', mode='a', maxBytes=12**20)
+    f = logging.Formatter('{asctime} - {processName} - {name} - {message}', style='{')
+    h.setFormatter(f)
+    root.addHandler(h)
+
+def listener_process(queue, configurer):
+    configurer()
+    while True:
+        try:
+            record = queue.get()
+            if record is None:
+                break
+            logger = logging.getLogger(record.name)
+            logger.handle(record)
+        except Exception:
+            import sys, traceback
+            print('Error!', file=sys.stderr)
+            traceback.print_exc(file=sys.stderr)
+
+
+# configure logger handler for each worker process
+def worker_configurer(queue):
+    h = logging.handlers.QueueHandler(queue)
+    root = logging.getLogger()
+    root.addHandler(h)
+    root.setLevel(logging.INFO) # send all messages to queue
+
+def worker_process(queue, configurer, localserverport, seeder=False, \
+    remoteserverport=None, hostname='127.0.0.1', torrent_file='Mozart_mininova.torrent'):
+    configurer(queue)
+    name = multiprocessing.current_process().name
+    print('Worker started: {}'.format(name))
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(enabled=True)
+
+    # create client
+    client = Client(TorrentWrapper(torrent_file), seeder=seeder)
+
+    # schedule client
+    loop.create_task(main(client, remoteserverport))
+
+    # create and schedule server
+    # server runs on (hostname, localserverport)
+    port=localserverport
+    server_coro = asyncio.start_server(client.handle_leecher, host='127.0.0.1', port=port)
+    server = loop.create_task(server_coro) # schedule it
+    logger.info('server is running on {}:{}'.format(hostname, port))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt as e:
+        logger.info('server connections open: {}'.format(server.sockets))
+        logger.info('closing server...')
+    finally:
+        # shutdown server (connection listener)
+        client.shutdown()
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
+
+
+
+def main_log():
+    queue = multiprocessing.Queue(-1)
+    listener = multiprocessing.Process(target=listener_process, args=(queue, listener_configurer))
+    listener.start()
+
+    seeder_16329 = multiprocessing.Process(target=worker_process, \
+        args=(queue, worker_configurer, 16329, True))
+    leecher_16350 = multiprocessing.Process(target=worker_process, \
+        args=(queue, worker_configurer, 16350, False, 16329))
+    leecher_16351 = multiprocessing.Process(target=worker_process, \
+        args=(queue, worker_configurer, 16351, False, 16350)) 
+    workers = [seeder_16329, leecher_16350, leecher_16351]
+    for worker in workers:
+        worker.start()
+    for worker in workers:
+        worker.join()
+    queue.put_nowait(None) # sentinel
+    listener.join()
+
+#################### 
+if __name__ == '__main__':
+    main_log()
